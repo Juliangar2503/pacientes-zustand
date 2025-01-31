e5d30494388eaf68ad1b7abbcec42385e5f53f71
juliangar2503 <jgq325@gmail.com>
2025-01-31 15:03:19 +0100

    first commit

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,38 @@
+import { usePatientStore } from "../store"
+import { Patient } from "../types"               
+import PatientDetailItem from "./PatientDetailItem"
+
+type PatientDetailsProps = {
+  patients: Patient
+}
+
+
+export default function PatientDetails({ patients }: PatientDetailsProps) {
+
+const deletePatient = usePatientStore((state) => state.deletePatient)
+const getPatientById = usePatientStore((state) => state.getPatientById)
+
+  return (
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+        <PatientDetailItem label="ID" data={patients.id} />
+        <PatientDetailItem label="Nombre" data={patients.name} />
+        <PatientDetailItem label="Propietario" data={patients.caretaker} />
+        <PatientDetailItem label="Email" data={patients.email} /> 
+        <PatientDetailItem label="Síntomas" data={patients.symptoms} />
+        <div className="flex justify-between mt-10 ">
+            <button
+                type="button"
+                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white fonst-bold uppercase rounded-lg"
+                onClick={() => getPatientById(patients.id)}
+            >Editar</button>
+            <button
+                type="button"
+                className="ml-3 py-2 px-10 bg-red-600 hover:bg-red-700 text-white fonst-bold uppercase rounded-lg"
+                onClick={() => deletePatient(patients.id)}
+            >Eliminar</button>
+
+        </div>
+    </div>
+
+  )
+}
